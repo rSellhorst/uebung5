d6dbda398246f1a6fa6a5fcacbbf0025becc1799
Romy Sellhorst <romysellhorst@mac.speedport.ip>
2025-11-21 10:39:54 +0100

    uebung5

--- src/h2/H2_main.java
+++ src/h2/H2_main.java
@@ -4,18 +4,20 @@ public class H2_main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-    int n=19;
+    int n=6709;
     int digits=0;
     int []a= {0,0,0,0,0,0,0,0,0};
-    digits=String.valueOf(n).length();
-    digits=0;
     
+    digits=0;
+    digits=String.valueOf(n).length();
+  
+    //übrigbleibenden Nullen erschaffen
     for (int i=0; i<(9-digits);i++) {
-    	a[1]=0;  
+    	a[i]=0;  
     System.out.println(a[i]); }
 	
-  
-    for(int i=7;i>=9-digits; i--) {
+    //Ziffern von n
+    for(int i=8;i>=9-digits; i--) {
     	a[i]=n%10;
     	n=n/10;}
     
